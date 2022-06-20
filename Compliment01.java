4ebb006da90ff476b106a469997ced2ad89fc1d9
ayana bando <ayanabando@gmail.com>
2022-06-20 12:57:59 -0400

    initial commit for ch4 lab

---
+++ Compliment01.java
@@ -0,0 +1,51 @@
+/**
+ * Description
+ *
+ * @author Student Name
+ * @since Date Started
+ */
+
+import java.util.Scanner;
+
+public class Compliment01 {
+    public static void main (String[] args) {
+        
+    // ========== INSERT YOUR CODE HERE ==========
+
+    Scanner sc = new Scanner(System.in);
+    //ask for input and store in variable 'age'
+    System.out.println("What is your age? ");
+    int age = sc.nextInt();
+
+    //move to next line for next input
+    sc.nextLine();
+
+    //ask for input and store in variable 'name'
+    System.out.println("What is your name?");
+    String name = sc.nextLine();
+
+    //output user entered name, age, and compliment
+    System.out.println("Hello, " + name + ". You are very smart for someone who is " + age + " years old.");
+    
+
+    
+    
+    
+    
+    // ===========================================        
+
+    }
+}
+
+
+
+
+
+
+// =============== LAB SUMMARY ===============
+
+/*
+
+
+
+*/
